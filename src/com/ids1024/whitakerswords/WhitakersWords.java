dbf52bc7d57257d174258bdb834cea81d9af57e3
Ian D. Scott <ian@perebruin.com>
2015-09-05 19:08:20 -0700

    Update action bar

--- src/com/ids1024/whitakerswords/WhitakersWords.java
+++ src/com/ids1024/whitakerswords/WhitakersWords.java
@@ -1,6 +1,9 @@
 package com.ids1024.whitakerswords;
 
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.os.Bundle;
 
 public class WhitakersWords extends Activity
@@ -12,4 +15,27 @@ public class WhitakersWords extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                // TODO
+                return true;
+            case R.id.action_settings:
+		// TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+    }
+}
 }
