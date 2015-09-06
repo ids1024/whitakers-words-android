4a003a8c1e0b97f86d5890b8c7cf3957256984da
Ian D. Scott <ian@perebruin.com>
2015-09-05 18:25:13 -0700

    Initial commit

---
+++ src/com/ids1024/whitakerswords/WhitakersWords.java
@@ -0,0 +1,15 @@
+package com.ids1024.whitakerswords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhitakersWords extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
