94cb20288d13adbd05f936c6a573834d9b4a3213
Ian D. Scott <ian@perebruin.com>
2015-09-07 15:05:45 -0700

    Improve ui; add and install words binary and data

--- src/com/ids1024/whitakerswords/WhitakersWords.java
+++ src/com/ids1024/whitakerswords/WhitakersWords.java
@@ -1,18 +1,55 @@
 package com.ids1024.whitakerswords;
 
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.lang.RuntimeException;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
 import android.os.Bundle;
 
+import android.util.Log;
+
 public class WhitakersWords extends Activity
 {
     /** Called when the activity is first created. */
+
+    public void copyFiles() throws IOException {
+        String[] filenames = {"ADDONS.LAT", "DICTFILE.GEN", "EWDSFILE.GEN",
+                "INDXFILE.GEN", "INFLECTS.SEC", "STEMFILE.GEN",
+                "UNIQUES.LAT", "words"};
+        for (String filename: filenames) {
+            // Note: Android does not accept upper case in resource names, so it is converted
+            // And file extensions are stripped, so remove that
+            String resourcename = filename.toLowerCase();
+            if (resourcename.contains(".")) { 
+                resourcename = resourcename.substring(0, filename.lastIndexOf('.'));
+            }
+
+            int identifier = getResources().getIdentifier(resourcename, "raw", getPackageName());
+            InputStream ins = getResources().openRawResource(identifier);
+            byte[] buffer = new byte[ins.available()];
+            ins.read(buffer);
+            ins.close();
+            FileOutputStream fos = openFileOutput(filename, MODE_PRIVATE);
+            fos.write(buffer);
+            fos.close();
+        }
+        File wordsbin = getFileStreamPath("words");
+        wordsbin.setExecutable(true);
+    }
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            copyFiles();
+        } catch(IOException e) {
+                throw new RuntimeException("Copying data files failed.");
+        }
         setContentView(R.layout.main);
     }
 
