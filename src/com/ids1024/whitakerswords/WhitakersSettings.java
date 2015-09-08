e2c069136e3cdde755fdf3e9ee4e1d39ce12bfaf
Ian D. Scott <ian@perebruin.com>
2015-09-07 21:10:32 -0700

    Allow getting out of setting by using up in action bar

--- src/com/ids1024/whitakerswords/WhitakersSettings.java
+++ src/com/ids1024/whitakerswords/WhitakersSettings.java
@@ -2,11 +2,15 @@ package com.ids1024.whitakerswords;
 
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.app.ActionBar;
 
 public class WhitakersSettings extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
