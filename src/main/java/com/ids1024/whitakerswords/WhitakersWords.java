5cb3c0f8cc92391f50f32a61fe9d5ab268e09b9a
Ian Douglas Scott <ian@iandouglasscott.com>
2017-01-08 19:31:45 -0800

    Add option to search on keypress, without hitting search button

--- src/main/java/com/ids1024/whitakerswords/WhitakersWords.java
+++ src/main/java/com/ids1024/whitakerswords/WhitakersWords.java
@@ -30,6 +30,8 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.text.SpannableStringBuilder;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.TextUtils;
 import android.text.style.StyleSpan;
 import android.text.style.ForegroundColorSpan;
@@ -324,6 +326,24 @@ public class WhitakersWords extends ListActivity
                     handled = true;
                 }
                 return handled;
+            }
+        });
+
+        search_term_view.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (getPreferences().getBoolean("search_on_keypress", false)) {
+                    search_term = search_term_view.getText().toString();
+                    searchWord();
+                }
             }
         });
 
