586769ca0dbd615ebc2d70668dcfce9999d08060
Ian Douglas Scott <ian@iandouglasscott.com>
2018-09-02 23:04:58 -0700

    Make assets directory

--- build-words-docker.sh
+++ build-words-docker.sh
@@ -5,6 +5,7 @@ set -e
 IMAGE=ids1024/ada-android:6.4.0
 
 sudo docker pull $IMAGE
+
 # Disable seccomp for 'personality' syscall, which is used by bionic (in qemu)
 sudo docker run \
 	-v "$PWD/words:/words" \
@@ -13,6 +14,8 @@ sudo docker run \
 	--security-opt seccomp=unconfined \
 	$IMAGE \
 	./build-words.sh
+
 rm -rf src/main/assets/words
+mkdir -p src/main/assets
 mv words/words src/main/assets/words
 echo "Words built in src/main/assets/words."
